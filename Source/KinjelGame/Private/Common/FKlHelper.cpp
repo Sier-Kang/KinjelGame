9a7a78f0b711dcdcf7ca5fa37a9bd64269d6945d
Sier-Kang <736484759@qq.com>
2018-10-26 14:05:44 +0800

    [ADD] Game option menu.

---
+++ Source/KinjelGame/Private/Common/FKlHelper.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FKlHelper.h"
+
+FKlHelper::FKlHelper()
+{
+}
+
+FKlHelper::~FKlHelper()
+{
+}
---
+++ Source/KinjelGame/Private/UI/Widgets/SKlGameOptionWidget.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SKlGameOptionWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SKlGameOptionWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
