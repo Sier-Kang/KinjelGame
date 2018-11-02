f86e8aa28ab2ad3319f1614c21bebe8e0310ad1d
Sier-Kang <736484759@qq.com>
2018-11-02 18:01:03 +0800

    [ADD] Game HUD shortcut menu

---
+++ Source/KinjelGame/Private/UI/Widgets/SKlGameHUDWidget.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SKlGameHUDWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SKlGameHUDWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
