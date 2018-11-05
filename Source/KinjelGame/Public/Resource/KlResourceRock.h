f528fe86bb9a5b6255fbfd2a49dacd95f002aa42
Sier-Kang <736484759@qq.com>
2018-11-05 17:58:17 +0800

    [ADD] Resource objects.

---
+++ Source/KinjelGame/Public/Resource/KlResourceRock.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Resource/KlResourceObject.h"
+#include "KlResourceRock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINJELGAME_API AKlResourceRock : public AKlResourceObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
