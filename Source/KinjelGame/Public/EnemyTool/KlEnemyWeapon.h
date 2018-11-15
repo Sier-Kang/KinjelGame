ad2696ecf17e173ed7dc389726d2b750b5b8a1e8
Sier-Kang <736484759@qq.com>
2018-11-15 17:48:26 +0800

    [ADD] Enemy's tool etc.

---
+++ Source/KinjelGame/Public/EnemyTool/KlEnemyWeapon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyTool/KlEnemyTool.h"
+#include "KlEnemyWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINJELGAME_API AKlEnemyWeapon : public AKlEnemyTool
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
