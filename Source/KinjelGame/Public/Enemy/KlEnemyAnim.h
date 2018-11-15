dd3c86fa55d9d3697b9b4cb3dd87a5041c2d8edd
Sier-Kang <736484759@qq.com>
2018-11-15 17:37:43 +0800

    [ADD] Enemy etc.

---
+++ Source/KinjelGame/Public/Enemy/KlEnemyAnim.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "KlEnemyAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINJELGAME_API UKlEnemyAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
