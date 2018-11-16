b0023dc36a86526b3d31f927e199a3b56363db53
Sier-Kang <736484759@qq.com>
2018-11-16 18:00:57 +0800

    [ADD] AI System etc created.

---
+++ Source/KinjelGame/Public/AI/KlBTDecoratorBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "KlBTDecoratorBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINJELGAME_API UKlBTDecoratorBase : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/KinjelGame/Public/AI/KlEnemyBlackboard.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "KlEnemyBlackboard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINJELGAME_API UKlEnemyBlackboard : public UBlackboardData
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
